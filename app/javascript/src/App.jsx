d618c51bbf5f2501b8c8d4f63213dd2d0fb48536
Vysakh-Poulose <vyshak.poulose@gmail.com>
2023-05-26 18:44:42 +0530

    Setup translation

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { AuthProvider } from "contexts/auth";
 import { UserProvider } from "contexts/user";
 
+import "./common/i18n";
 import Main from "./components/Main";
 
 const App = props => (
