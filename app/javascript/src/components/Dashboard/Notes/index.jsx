3d5ed523e0f662d2f4871afd626dd440c2b4594d
Vysakh-Poulose <vyshak.poulose@gmail.com>
2023-05-26 23:58:40 +0530

    Removed unused alert and states

--- app/javascript/src/components/Dashboard/Notes/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/index.jsx
@@ -9,7 +9,6 @@ import { useTranslation } from "react-i18next";
 import notesApi from "apis/notes";
 import EmptyState from "components/commons/EmptyState";
 
-import DeleteAlert from "./DeleteAlert";
 import List from "./List";
 import Menubar from "./Menubar";
 import NewNotePane from "./Pane/Create";
@@ -19,9 +18,7 @@ const Notes = () => {
   const [loading, setLoading] = useState(true);
   const [showNewNotePane, setShowNewNotePane] = useState(false);
   const [showMenu, setShowMenu] = useState(true);
-  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const [selectedNoteIds, setSelectedNoteIds] = useState([]);
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
@@ -83,14 +80,6 @@ const Notes = () => {
           setShowPane={setShowNewNotePane}
           showPane={showNewNotePane}
         />
-        {showDeleteAlert && (
-          <DeleteAlert
-            refetch={fetchNotes}
-            selectedNoteIds={selectedNoteIds}
-            setSelectedNoteIds={setSelectedNoteIds}
-            onClose={() => setShowDeleteAlert(false)}
-          />
-        )}
       </Container>
     </>
   );
